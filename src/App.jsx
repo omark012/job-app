f3bb2c1a602cba45415661990221c4b2c6711a23
omark012 <omarasif75@gmail.com>
2024-07-11 17:20:51 +0300

    updated addJobPage

--- src/App.jsx
+++ src/App.jsx
@@ -13,20 +13,25 @@ import AddJobPage from "./pages/AddJobPage";
 import NotFoundPage from "./pages/NotFoundPage";
 import JobPage, { jobLoader } from "./pages/JobPage";
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<MainLayout />}>
-      <Route index element={<HomePage />} />
-      <Route path="/jobs" element={<JobsPage />} />
-      <Route path="/add-job" element={<AddJobPage />} />
-      <Route path="/job/:id" element={<JobPage />} loader={jobLoader} />
-      <Route path="*" element={<NotFoundPage />} /> // route to notfoundpage if
-      wrong path is given
-    </Route>
-  )
-);
-
 const App = () => {
+  const addJobSubmit = (newJob) => {};
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<HomePage />} />
+        <Route path="/jobs" element={<JobsPage />} />
+        <Route
+          path="/add-job"
+          element={<AddJobPage addJobSubmit={addJobSubmit} />}
+        />
+        <Route path="/job/:id" element={<JobPage />} loader={jobLoader} />
+        <Route path="*" element={<NotFoundPage />} /> // route to notfoundpage
+        if wrong path is given
+      </Route>
+    )
+  );
+
   return <RouterProvider router={router} />;
 };
 
